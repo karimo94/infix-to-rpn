0225cc8b508fb02a8b8b1d87965076c88a174ae8
Karim Oumghar <misteralgeria77@yahoo.fr>
2015-01-03 22:19:43 -0800

    Version 1.0
    Evaluates an infix expression and converts to prefix.

--- Program.cs
+++ Program.cs
@@ -9,24 +9,25 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            string infix = "3^4+(11-(3*2))/4";
-            char[] tokens = infix.ToCharArray();
-            Stack<char> s = new Stack<char>();
-            List<char> outputList = new List<char>();
+            string infix = "3 ^ 4 + ( 11 - ( 3 * 2 ) ) / 2";
+            string[] tokens = infix.Split(' ');
+
+            Stack<string> s = new Stack<string>();
+            List<string> outputList = new List<string>();
             int n;
-            foreach (char c in tokens)
+            foreach (string c in tokens)
             {
                 if(int.TryParse(c.ToString(), out n))
                 {
                     outputList.Add(c);
                 }
-                if (c == '(')
+                if (c == "(")
                 {
                     s.Push(c);
                 }
-                if (c == ')')
+                if (c == ")")
                 {
-                    while (s.Count != 0 && s.Peek() != '(')
+                    while (s.Count != 0 && s.Peek() != "(")
                     {
                         outputList.Add(s.Pop());
                     }
@@ -34,31 +35,35 @@ namespace ConsoleApplication1
                 }
                 if (isOperator(c) == true)
                 {
-                    while(s.Count != 0 || Priority(s.Peek()) >= Priority(c))//here OR used to be AND
+                    while(s.Count != 0 && Priority(s.Peek()) >= Priority(c))
                     {
                         outputList.Add(s.Pop());
                     }
                     s.Push(c);
                 }
             }
-            
+            while (s.Count != 0)//if any operators remain in the stack, pop all & add to output list until stack is empty 
+            {
+                outputList.Add(s.Pop());
+            }
             for (int i = 0; i < outputList.Count; i++)
             {
                 Console.Write("{0}", outputList[i]);
             }
+            
             Console.ReadLine();
         }
-        static int Priority(char c)
+        static int Priority(string c)
         {
-            if (c == '^')
+            if (c == "^")
             {
                 return 3;
             }
-            else if (c == '*' || c == '/')
+            else if (c == "*" || c == "/")
             {
                 return 2;
             }
-            else if (c == '+' || c == '-')
+            else if (c == "+" || c == "-")
             {
                 return 1;
             }
@@ -67,9 +72,9 @@ namespace ConsoleApplication1
                 return 0;
             }
         }
-        static bool isOperator(char c)
+        static bool isOperator(string c)
         {
-            if (c == '+' || c == '-' || c == '*' || c == '/' || c == '^')
+            if (c == "+" || c == "-" || c == "*" || c == "/" || c == "^")
             {
                 return true;
             }
